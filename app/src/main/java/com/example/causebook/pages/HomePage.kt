f635113fa2ca6e17c735114841c02f001396ad2d
Vigneshwaren333 <vigneshwaren5053@gmail.com>
2025-05-07 18:54:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/causebook/pages/HomePage.kt
@@ -0,0 +1,8 @@
+package com.example.causebook.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier) {
+}
